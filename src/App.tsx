a63a9b7c3f521ee1d52bf8e2cd5461560b50daf1
Nicolas Arriagada <flavia.camila.dasilva27@gmail.com>
2023-11-09 16:18:59 -0300

    Implementação de criar, atualizar e deletar temas

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ import Login from './pages/login/Login';
 import Footer from './pages/components/footer/Footer';
 import Navbar from './pages/components/navbar/Navbar';
 import Cadastro from './pages/cadastro/Cadastro';
+import ListaTemas from './pages/components/temas/listaTemas/ListaTemas';
+import FormularioTema from './pages/components/temas/formularioTema/FormularioTema';
+import DeletarTema from './pages/components/temas/deletarTema/DeletarTema';
+
+
 
 function App() {
     return (
@@ -18,6 +23,10 @@ function App() {
                         <Route path="/login" element={<Login />} />
                         <Route path="/home" element={<Home />} />
                         <Route path="/cadastro" element={<Cadastro />} />
+                        <Route path="/temas" element={<ListaTemas />} />
+                        <Route path="/cadastroTema" element={<FormularioTema />} />
+                        <Route path="/editarTema/:id" element={<FormularioTema />} />
+                        <Route path="/deletarTema/:id" element={<DeletarTema />} />
                     </Routes>
                 </div>
                 <Footer />
--- src/pages/components/footer/Footer.tsx
+++ src/pages/components/footer/Footer.tsx
@@ -6,18 +6,20 @@ function Footer() {
 
     return (
         <>
+        <div className="flex flex-col justify-center items-center bg-[#8DA8E0] text-white ">
             <div className="flex justify-center bg-indigo-900 text-white">
-                <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>
+                <div className="container flex flex-col items-center py-4 bg-[#8DA8E0]">
+                    <p className='text-xl font-bold bg-[#8DA8E0]'>
                             Blog Pessoal Generation | Copyright: {data}
-                        </p>
+                        </p>        
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
+                    <a href="https://www.linkedin.com/" target='_blank'><LinkedinLogo size={48} className='stroke-2 text-[#8830E6] hover:scale-110 transition-all'/></a>
+                                <a href="https://www.instagram.com/" target='_blank'><InstagramLogo size={48} className='stroke-2 text-[#8830E6] hover:scale-110 transition-all'/></a>
+                                <a href="https://www.facebook.com/" target='_blank'><FacebookLogo size={48} className='stroke-2 text-[#8830E6] hover:scale-110 transition-all'/></a>
                     </div>
                 </div>
+            </div>
             </div>
         </>
     )
